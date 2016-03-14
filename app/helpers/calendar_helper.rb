25f94bed42fa095f456c638f20ab7d0d06aaf67b
Gregory Wehmeier <wehmenstein@gmail.com>
2016-03-13 23:22:22 -0700

    fix multiple times on same day. fix error in counting days last month

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,3 +1,4 @@
+
 module CalendarHelper
   require 'date'
 
@@ -5,8 +6,10 @@ module CalendarHelper
     # today_obj = DateTime.new(2016,03,5)
     today_obj = DateTime.now()
     month_starts_on = DateTime.new(today_obj.year, today_obj.month, 1).wday
+    puts "MONTH_STARTS_ON"
+    puts "=#{month_starts_on}"
     if (month_starts_on != 0)
-      calendar_nums = filler_days(today_obj)
+      calendar_nums = filler_days(today_obj, month_starts_on)
     end
 
     calendar_nums ||= []
@@ -17,12 +20,23 @@ module CalendarHelper
     return calendar_nums
   end
 
-  def filler_days(today_obj)
+  def filler_days(today_obj, month_starts_on)
+    puts "="*50
+    puts "FILLER DAYS"
     prev_month_str = Date::MONTHNAMES[today_obj.prev_month.month].downcase
 
+    puts today_obj
+    puts today_obj.wday
+
     prev_month_days = num_days_last_month(today_obj)
-    first_filler_date = ( prev_month_days - ( today_obj.wday - 1 ))
+    puts "PREV_MONTH_DAYS"
+    puts prev_month_days
+    first_filler_date = ( prev_month_days - ( month_starts_on - 1 ))
+    puts "FIRST_FILLER_DATE"
+    puts first_filler_date
     dates_from_last_month = Array(first_filler_date..prev_month_days)
+    puts "DATES_FROM_LAST_MONTH"
+    puts dates_from_last_month
     dates_from_last_month.map!{|date|Array[date,prev_month_str]}
   end
 
--- app/models/screening.rb
+++ app/models/screening.rb
@@ -12,14 +12,21 @@ class Screening < ActiveRecord::Base
       showtime = Screening.date_time_to_string(screening_obj.date_time).downcase
       showtime = showtime.slice(1,showtime.length+1)
 
-      screening_info = {
-      :movie => screening_obj.film.title,
-      :showtime => showtime,
-      :theater => screening_obj.theater.name
-    }
+      screening_inf = {
+        :movie => screening_obj.film.title,
+        :showtime => showtime,
+        :theater => screening_obj.theater.name
+      }
       mday = screening_obj.date_time.mday.to_s
       screenings_hash[mday] ||= []
-      screenings_hash[mday] << screening_info
+
+      if screenings_hash[mday].any?{|info|info.has_value?(screening_inf[:movie])}
+        existing_showtime_info = screenings_hash[mday].select{|info|info[:movie] == screening_inf[:movie]}.first
+        existing_showtime_info[:showtime] << "|#{screening_inf[:showtime]}"
+      else
+        screenings_hash[mday] << screening_inf
+      end
+
     end
     screenings_hash
   end
@@ -29,3 +36,19 @@ class Screening < ActiveRecord::Base
     date_time.strftime("%I:%M%p")
   end
 end
+
+
+
+
+
+# screenings_hash[mday].each do |info|
+#   if (info.has_value?(screening_inf[:movie]))
+#     info[:showtime] << "|#{screening_inf[:showtime]}"
+#     screening_inf = {}
+#     break
+#   end
+# end
+
+
+
+
