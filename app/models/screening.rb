07e8d9cb4b70ee3184920558e57c3321054a5173
Gregory Wehmeier <wehmenstein@gmail.com>
2016-03-13 23:45:37 -0700

    fix time formatting error, add links to movie listings

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -10,19 +10,21 @@ class Screening < ActiveRecord::Base
 
     screenings_arr.each do |screening_obj|
       showtime = Screening.date_time_to_string(screening_obj.date_time).downcase
-      showtime = showtime.slice(1,showtime.length+1)
+
+
 
       screening_inf = {
         :movie => screening_obj.film.title,
         :showtime => showtime,
-        :theater => screening_obj.theater.name
+        :theater => screening_obj.theater.name,
+        :ticketing_url => screening_obj.ticketing_url
       }
       mday = screening_obj.date_time.mday.to_s
       screenings_hash[mday] ||= []
 
       if screenings_hash[mday].any?{|info|info.has_value?(screening_inf[:movie])}
         existing_showtime_info = screenings_hash[mday].select{|info|info[:movie] == screening_inf[:movie]}.first
-        existing_showtime_info[:showtime] << "|#{screening_inf[:showtime]}"
+        existing_showtime_info[:showtime] << " | #{screening_inf[:showtime]}"
       else
         screenings_hash[mday] << screening_inf
       end
@@ -33,21 +35,12 @@ class Screening < ActiveRecord::Base
 
   def self.date_time_to_string(date_time)
     # d.strftime("at %I:%M%p")
-    date_time.strftime("%I:%M%p")
+    time = date_time.strftime("%I:%M%p")
+    return time.slice(1,time.length) if time [0] == "0"
+    time
   end
 end
 
-
-
-
-
-# screenings_hash[mday].each do |info|
-#   if (info.has_value?(screening_inf[:movie]))
-#     info[:showtime] << "|#{screening_inf[:showtime]}"
-#     screening_inf = {}
-#     break
-#   end
-# end
 
 
 
