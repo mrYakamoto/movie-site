9ca39b35a088b77d6d24ee79ec5e91a51fe5dbc0
Gregory Wehmeier <wehmenstein@gmail.com>
2016-03-19 13:55:37 -0700

    dynamically remove listing from watchlist. begin logic to update all listings for a film when switching tooltip content

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,21 +4,9 @@ $('document').ready(function(){
   addBuzzBoxPosterListener();
 
   addAllToolTips();
-  addBackgroundToEmptyDays();
+  addBackgroundToAllEmptyDays();
 });
 
-function cookieValue(){
-  var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-  return cookieValue;
-}
-
-function userLoggedIn(){
-  if (cookieValue()){
-    return true;
-  } else {
-    return false;
-  }
-}
 
 function addBuzzBoxPosterListener(){
   $('div.thumbnail').hover(
@@ -30,8 +18,75 @@ function addBuzzBoxPosterListener(){
     })
 }
 
-function addAllToolTips(){
 
+function addBackgroundToAllEmptyDays(){
+  var $empty_dates = $( 'td.date-box:has( > span.placeholder )');
+  $empty_dates.addClass('gradient')
+}
+
+
+
+// AJAX
+
+function addFilmToWatchlist(data){
+  $.ajax({
+    method: 'POST',
+    url: '/users_films',
+    data: data
+  })
+  .done(function(response){
+    flashAjaxResponse(response);
+  })
+  .error(function(xhr, unknown, error){
+    alert(error);
+  })
+}
+
+function removeFilmFromWatchlist(data){
+  $.ajax({
+    method: 'DELETE',
+    url: "/users_films/0",
+    data: data
+  })
+  .done(function(response){
+    flashAjaxResponse(response);
+    removeWatchlistListings(data["film_id"]);
+  })
+  .error(function(xhr, unknown, error){
+    alert(error);
+  })
+}
+
+function flashAjaxResponse(response){
+  if ( response.success ){
+    flashSuccess(response.success)
+  }
+  else if ( response.errors.film_id ){
+    flashError(response.errors.film_id[0])
+  }
+  else
+    console.log("ERROR");
+}
+
+function flashError(text){
+  $("div.alert-danger").empty().prepend(text).slideDown(150);
+  setTimeout(function() {
+    $("div.alert-danger").slideUp(150);
+  }, 2000);
+}
+
+function flashSuccess(text){
+  $("div.alert-success").empty().prepend(text).slideDown(150);
+  setTimeout(function() {
+    $("div.alert-success").slideUp(150);
+  }, 2000);
+}
+
+// END OF AJAX
+
+// TOOLTIPS
+
+function addAllToolTips(){
   console.log("ADD ALL TOOL TIPS");
   if (userLoggedIn()){
     console.log("USER LOGGED IN");
@@ -59,18 +114,15 @@ function addTooltip(){
       effect: function(offset) {
         $( this ).slideUp( 100 )
       },
-      inactive: 1300
+      inactive: 20000
     },
-    style: {
-      classes: 'qtip-tipsy'
-    },
+    style: { classes: 'qtip-tipsy' },
     position: {
       my: 'top center',
       at: 'bottom center'
     }
   });
 }
-
 
 function addAllTooltipClickListeners(){
   console.log("ADD ALL TOOLTIP CLICK LISTENERS");
@@ -83,6 +135,7 @@ function addAllTooltipClickListeners(){
 function addTooltipClickListener(){
   console.log("ADD TOOLTIP CLICK LISTENER")
   var toolTipContent = this
+
   var data = {film_id: toolTipContent.id};
 
   $(toolTipContent).click(function(){
@@ -92,6 +145,7 @@ function addTooltipClickListener(){
     } else if (isOnWatchlist.call(toolTipContent)){
       removeFilmFromWatchlist(data);
     }
+    $(toolTipContent).off('click');
     switchTooltips.call(toolTipContent);
   })
 }
@@ -108,115 +162,55 @@ function isOnWatchlist(){
   }
 }
 
-
-
 function switchTooltips(){
   console.log("SWITCH TOOLTIPS");
-  var toolTipContent = this;
-  var $toolTipContent = $(toolTipContent);
+  var tooltipContent = this
+  var $tooltipContent = $(this);
+  var $allFilmsToolTips = $('div.tooltipContent#'+$tooltipContent.attr('id'))
 
-  debugger
+  // var qtipId = $toolTipContent.closest('div.qtip').attr('data-qtip-id');
+  // var target = $('span[data-hasqtip='+qtipId+']');
+  // var api = $(target).qtip('api');
 
-  var qtipId = $toolTipContent.closest('div.qtip').attr('data-qtip-id');
-  var target = $('span[data-hasqtip='+qtipId+']');
-  var api = $(target).qtip('api');
 
-  $toolTipContent.toggleClass("add-film remove-film");
-  addTooltipClickListener.call(toolTipContent);
-// replace content of qtip
-// target.qtip('option', 'content.text', "add to favorites");
+  $tooltipContent.toggleClass("add-film remove-film");
+  addTooltipClickListener.call(tooltipContent);
+
+
+  // $allFilmsToolTips.each(function(){
+  //   var tooltipContent = this
+
+  //   $(tooltipContent).toggleClass("add-film remove-film");
+  //   addTooltipClickListener.call(tooltipContent);
+  // })
+
 }
 
-function addFilmToWatchlist(data){
-  $.ajax({
-    method: 'POST',
-    url: '/users_films',
-    data: data
-  })
-  .done(function(response){
-    flashAjaxResponse(response);
-  })
-  .error(function(xhr, unknown, error){
-    alert(error);
-  })
-}
-
-function removeFilmFromWatchlist(data){
-  $.ajax({
-    method: 'DELETE',
-    url: "/users_films/0",
-    data: data
-  })
-  .done(function(response){
-    flashAjaxResponse(response);
-  })
-  .error(function(xhr, unknown, error){
-    alert(error);
-
+function removeWatchlistListings(film_id){
+  var $filmListings = $("body.users.show div.film-listing-container#film-"+film_id)
+  var $dateBoxes = $filmListings.parent();
+  $filmListings.remove();
+  $dateBoxes.each(function(){
+    if ($(this).has('div.film-listing-container').length === 0){
+      $(this).append("<br><span class='placeholder hidden-sm hidden-md hidden-lg hidden-xl'></span>");
+      $(this).addClass('gradient')
+    }
   })
 }
 
-function addWatchlistTooltips(){
-  if (userLoggedIn()){
+// END OF TOOLTIPS
 
-    $('body.users.show .hasTooltip').each(function(){
-      $(this).qtip({
-        content: $(this).next('.tooltipContent:first'),
-        show: {
-          event: 'click',
-          effect: function(offset) {
-            $( this ).slideDown( 100 )
-          }
-        },
-        hide: {
-          event: 'unfocus',
-          effect: function(offset) {
-            $( this ).slideUp( 100 )
-          },
-          inactive: 1300
-        },
-        style: {
-          classes: 'qtip-tipsy'
-        },
-        position: {
-          my: 'top center',
-          at: 'bottom center'
-        }
-      });
-    });
+// USERS
+function cookieUserValue(){
+  var cookieUserValue = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  return cookieUserValue;
+}
+
+function userLoggedIn(){
+  if (cookieUserValue()){
+    return true;
+  } else {
+    return false;
   }
 }
-
-function flashAjaxResponse(response){
-
-  if ( response.success ){
-    flashSuccess(response.success)
-  }
-  else if ( response.errors.film_id ){
-    flashError(response.errors.film_id[0])
-  }
-  else
-    console.log("ERROR");
-}
-
-function flashError(text){
-  $("div.alert-danger").prepend(text).slideDown(150);
-  setTimeout(function() {
-    $("div.alert-danger").empty().slideUp(150);
-  }, 2000);
-}
-
-function flashSuccess(text){
-  $("div.alert-success").prepend(text).slideDown(150);
-  setTimeout(function() {
-    $("div.alert-success").empty().slideUp(150);
-  }, 2000);
-}
-
-
-function addBackgroundToEmptyDays(){
-  var $empty_dates = $( 'td.date-box:has( > span.placeholder )');
-  $empty_dates.addClass('gradient')
-}
-
-
+// END OF USERS
