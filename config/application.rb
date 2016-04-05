eaef554f50c85e5041e0caad1c0d1ffa3d1b73d4
Gregory Wehmeier <wehmenstein@gmail.com>
2016-04-05 01:33:01 -0700

    precompile

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,9 @@ module MovieBoard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += [
+ '*.js','*.css', '*.scss'
+ ]
   end
 end
